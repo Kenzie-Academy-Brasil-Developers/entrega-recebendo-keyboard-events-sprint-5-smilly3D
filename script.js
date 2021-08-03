2b80a34d61effec6d26eb7220732f44ebcbb5ff3
Smilly Notbook <smilly.3d@gmail.com>
2021-08-03 12:29:44 -0300

    finalizado

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+let boxTop = 200;
+let boxLeft = 200;
+let box = document.getElementById("box")
+let move = document.querySelector('.move')
+
+
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    if(keyName === 'ArrowDown'){
+        boxTop += 10
+        box.style.top = boxTop + "px";
+        move.setAttribute('src', 'pacmanDown.png')
+    }
+    if(keyName === 'ArrowUp'){
+        boxTop -= 10
+        box.style.top = boxTop + "px";
+        move.setAttribute('src', 'pacmanUp.png')
+
+    }
+    if(keyName === 'ArrowLeft'){
+        boxLeft -= 10
+        box.style.left = boxLeft + "px";
+        move.setAttribute('src', 'pacmanLeft.png')
+    }
+    if(keyName === 'ArrowRight'){
+        boxLeft += 10
+        box.style.left = boxLeft + "px";
+        move.setAttribute('src', 'pacmanRigth.png')
+
+    }
+    console.log('keydown event\n\n' + 'key: ' + keyName);
+});
